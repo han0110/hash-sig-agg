7243f9b036263331c5b113c0f2831f45200e79cc
han0110 <tinghan0110@gmail.com>
2025-02-19 06:13:53 +0000

    feat: show proving time parts

--- circuit/openvm/examples/hash-sig-agg.rs
+++ circuit/openvm/examples/hash-sig-agg.rs
@@ -5,6 +5,13 @@ use hash_sig_agg_circuit_openvm::{
     util::engine::Engine,
 };
 use hash_sig_testdata::mock_vi;
+use metrics::Key;
+use metrics_tracing_context::TracingContextLayer;
+use metrics_util::{
+    debugging::{DebugValue, DebuggingRecorder, Snapshot},
+    layers::Layer,
+    CompositeKey, MetricKind,
+};
 use openvm_stark_backend::{engine::StarkEngine, prover::types::ProofInput};
 use std::time::{Duration, Instant};
 use tracing_forest::{util::LevelFilter, ForestLayer};
@@ -19,8 +26,6 @@ struct Args {
     log_signatures: usize,
     #[arg(long, short = 'p', default_value_t = 0)]
     proof_of_work_bits: usize,
-    #[arg(long, short)]
-    trace: bool,
 }
 
 fn main() {
@@ -47,21 +52,26 @@ fn main() {
         }
     }
 
-    if args.trace {
-        let env_filter = EnvFilter::builder()
-            .with_default_directive(LevelFilter::INFO.into())
-            .from_env_lossy();
+    let env_filter = EnvFilter::builder()
+        .with_default_directive(LevelFilter::WARN.into())
+        .from_env_lossy();
 
-        Registry::default()
-            .with(env_filter)
-            .with(ForestLayer::default())
-            .init();
-    }
+    Registry::default()
+        .with(env_filter)
+        .with(ForestLayer::default())
+        .init();
+
+    let recorder = DebuggingRecorder::new();
+    let snapshotter = recorder.snapshotter();
+    let recorder = TracingContextLayer::all().layer(recorder);
+    metrics::set_global_recorder(recorder).unwrap();
 
     let start = Instant::now();
     let (_, inputs) = generate_air_proof_inputs(args.log_blowup, vi);
+    let witgen_time = start.elapsed();
     let proof = engine.prove(&pk, ProofInput::new(zip(0.., inputs).collect()));
     let proving_time = start.elapsed();
+    let proving_time_parts = proving_time_parts(proving_time, witgen_time, snapshotter.snapshot());
 
     let start = Instant::now();
     engine.verify(&pk.get_vk(), &proof).unwrap();
@@ -72,8 +82,61 @@ fn main() {
 
     println!(
         r"proving time: {proving_time:.2?}
+{proving_time_parts}
 throughput: {throughput:.2} sigs/s
 proof_size: {proof_size_mb:.2} MB
-verifying time: {verifying_time:.2?}"
+verifying time: {verifying_time:.2?}",
     );
+}
+
+fn proving_time_parts(proving: Duration, witgen: Duration, snapshot: Snapshot) -> String {
+    #[allow(clippy::mutable_key_type)]
+    let snapshot = snapshot.into_hashmap();
+
+    let part = |name| {
+        let key = CompositeKey::new(MetricKind::Gauge, Key::from_name(name));
+        match snapshot[&key].2 {
+            #[allow(clippy::cast_sign_loss)]
+            DebugValue::Gauge(value) => Duration::from_millis(value.0 as u64),
+            _ => unreachable!(),
+        }
+    };
+    let [commit_main, compute_perm, commit_perm, compute_quot, commit_quot, opening] = [
+        "main_trace_commit_time_ms",
+        "generate_perm_trace_time_ms",
+        "perm_trace_commit_time_ms",
+        "quotient_poly_compute_time_ms",
+        "quotient_poly_commit_time_ms",
+        "pcs_opening_time_ms",
+    ]
+    .map(part);
+
+    let rest = proving
+        - (witgen
+            + commit_main
+            + compute_perm
+            + commit_perm
+            + compute_quot
+            + commit_quot
+            + opening);
+
+    let ratio = |n: Duration| 100.0 * n.as_secs_f64() / proving.as_secs_f64();
+    format!(
+        r"  witgen: {witgen:.2?} ({:02.2}%)
+  commit_main: {commit_main:?} ({:02.2}%)
+  compute_perm: {compute_perm:?} ({:02.2}%)
+  commit_perm: {commit_perm:?} ({:02.2}%)
+  compute_quot: {compute_quot:?} ({:02.2}%)
+  commit_quot: {commit_quot:?} ({:02.2}%)
+  opening: {opening:?} ({:02.2}%)
+  rest: {rest:.2?} ({:02.2}%)",
+        ratio(witgen),
+        ratio(commit_main),
+        ratio(compute_perm),
+        ratio(commit_perm),
+        ratio(compute_quot),
+        ratio(commit_quot),
+        ratio(opening),
+        ratio(rest)
+    )
 }
